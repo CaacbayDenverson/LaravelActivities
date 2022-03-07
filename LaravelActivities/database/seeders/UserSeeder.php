69cf2e93c434538644febb9343bc3f9e71201f47
DenversonCaacbay <denverkunfalcon@gmail.com>
2022-03-07 18:45:14 +0800

    Act 3

---
+++ LaravelActivities/database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' =>Str::random(15),
+            'email' => Str::random(15).'@gmail.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+
+        ]);
+    }
+}
